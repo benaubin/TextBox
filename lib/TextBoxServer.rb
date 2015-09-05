232b492d6843bab77ef3b1bdf97560c0ea4abac7
Ben <penne12@users.noreply.github.com>
2015-09-05 13:07:48 -0500

    Update TextBoxServer.rb

--- lib/TextBoxServer.rb
+++ lib/TextBoxServer.rb
@@ -4,11 +4,6 @@ require 'sinatra'
 class Server < Sinatra::Base
   puts 'Starting text box server!'
 
-#get '/' is like rails routes.rb, basically saying whenever you go to the homepage of the api.
-  get '/' do
-    File.read('text_box.txt')
-  end
-
 #put '/' is like get '/', but it is a bit different, and accepts a body (learn the difference between put and get here: http://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol#Request_methods)
   put '/' + File.read('text_box_password') do
     #Does things (you don't have to understand it - I'm not exactly sure either :P)
